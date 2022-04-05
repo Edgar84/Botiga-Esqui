62dd89fbb325962697420f620f64fd2c298d4a76
Edgar84 <serie.b23@gmail.com>
2022-04-05 16:44:20 +0200

    alxius principals

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Botiga Esquí</title>
+    <link rel="stylesheet" href="src/css/reset.css"/>
+    <link rel="stylesheet" href="src/css/bootstrap-4.6.1/bootstrap.min.css"/>
+    <link rel="stylesheet" href="src/css/fontawesome/all.min.css"/>
+    <link rel="stylesheet" href="src/css/style.css"/>
+</head>
+<body>
+    
+
+    <script src="src/js/functions.js"></script>
+    <script src="src/js/bootstrap-4.6.1/jquery3_6_0.slim.min.js"></script>
+    <script src="src/js/bootstrap-4.6.1/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ src/js/functions.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Funcions javascript generals');
+});
