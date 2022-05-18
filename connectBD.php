72e9f6e3cabb726737dfb0dc657905ecf4f60fb0
Edgar84 <serie.b23@gmail.com>
2022-05-18 18:25:32 +0200

    connect

--- connectBD.php
+++ connectBD.php
@@ -1,7 +1,8 @@
 <?php
     
     try {
-        $connect = new PDO("mysql:host=localhost;dbname=db_esqui", "root", "client");
+        //$connect = new PDO("mysql:host=localhost;dbname=db_esqui", "root", "client");
+        $connect = new PDO("mysql:host=localhost;dbname=db_esqui", "root", "12345");
     } catch (PDOException $e) {
         exit("Error: " . $e->getMessage());
     }
